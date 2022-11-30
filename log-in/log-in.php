c226faa6716bfcfe62dae504e7f67827ae5cb3f8
student <stanislav.medvec6@gmail.com>
2022-11-30 12:13:13 +0100

    start log-in.php page

---
+++ log-in/log-in.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>log-in</title>
+</head>
+<body>
+    <div id="content">
+        <div id="log-in">
+            <div id="log-in-header">
+                <div id="log-in-title"></div>
+                <a href="../index.html"></a>
+            </div>
+            <div id="log-in-content">
+                <input type="text">
+                <input type="text">
+            </div>
+            <div id="log-in-footer"></div>
+        </div>
+    </div>
+
+    <div id="log-in-header"></div>
+</body>
+</html>
