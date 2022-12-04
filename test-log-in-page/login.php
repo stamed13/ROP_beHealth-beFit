8912bb8cd729451d78dc4862088ef0d6aaf9a9f3
Stanislav Medvec <stanislav.medvec6@gmail.com>
2022-12-04 22:11:50 +0100

    crerated test login page with php

---
+++ test-log-in-page/login.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $uname = validate($_POST['username']);
+    $pass = validate($_POST['password']);
+
+} else {
+    header("Location: index.php");
+    exit();
+}
