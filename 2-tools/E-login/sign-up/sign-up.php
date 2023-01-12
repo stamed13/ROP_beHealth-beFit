a0c5461b04c029c9cd6230ca3a10ae0efb1dc5f6
Stanislav Medvec <stanislav.medvec6@gmail.com>
2023-01-12 23:28:02 +0100

    update contact responsivty, create sign-up page

--- 2-tools/E-login/sign-up/sign-up.php
+++ 2-tools/E-login/sign-up/sign-up.php
@@ -1,12 +1,77 @@
+<!--
+<?php
+    echo "<br>Ahoj php,<br> toto je log-in";
+?>
+-->
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="../../B-media/real-icon3.png" type="image/x-icon" />
+    <link rel="stylesheet" href="styles/sign-up.css">
+
+    <title>sign-up</title>
 </head>
 <body>
-    
+    <div id="content">
+        <div id="log-in">
+            <div id="log-in-header">
+                <div id="log-in-title">Sign-up</div>
+                <a href="../../../index.php"><img src="../../B-media/logo-real.svg" 
+                    alt="hopa" id="logo"></a>
+            </div>
+            <form action="" method="" id="log-in-formular">
+                <input type="email" id="email" name="email" 
+                placeholder="E-mail" required>
+
+                <input type="password" id="password" name="password" 
+                placeholder="Heslo" required>
+
+                <input type="password" id="co-password" name="co-password" 
+                placeholder="Potvrd heslo" required>
+
+                <input type="text" id="fname" name="fname" 
+                placeholder="Meno" required>
+
+                <input type="text" id="lname" name="lname" 
+                placeholder="Priezvisko" required>
+
+                <select id="gender" name="gender">
+                    <option value="0">Vyber pohlavie</option>
+                    <?php foreach($sizes as $size): ?>
+                        <option value="<?= $size["id"] ?>"
+                            <?php if($_POST["size"] == $size["id"]): ?>
+                                selected
+                            <?php endif ?>
+                        >
+                            <?= $size["name"] ?>
+                        </option>
+                    <?php endforeach ?>
+                </select>
+
+                <input type="number" id="age" name="age" 
+                placeholder="Vek" required>
+
+                <input type="number" id="height" name="height" 
+                placeholder="Vyska" required>
+
+                <input type="number" id="weight" name="weight" 
+                placeholder="Vaha" required>
+
+            </form>
+            <div id="log-in-footer">
+                <input type="submit" id="bt-login" name="submit" 
+                value="Register" required>
+                <a href="../log-in/log-in.php" id="new-account">Already have an account</a>
+            </div>
+        </div>
+    </div>
+
+    <div id="log-in-header"></div>
 </body>
 </html>
--- contact.php
+++ contact.php
@@ -7,22 +7,26 @@
 ?>
 
     <div id="content">
-        <div id="content-title">Contact</div>
-        <article id="autor-info">
-            <div id="title-autor">autor</div>
-            <div class="contact-item">
-                <div class="item-title">name:</div>
-                <div class="item-content" id="full-name">Stanislav Medvec</div>
-            </div>
-            <div class="contact-item">
-                <div class="item-title">email:</div>
-                <div class="item-content" id="email"></div>
-            </div>
-            <div class="contact-item">
-                <div class="item-title">github:</div>
-                <div class="item-content" id="github"></div>
-            </div>
-        </article>
+        <section id="contact">
+            <div id="content-title">Contact</div>
+            <article id="autor-info">
+                <div id="title-autor">autor</div>
+                <div id="contact-items">
+                <div class="contact-item">
+                        <div class="item-title">name:</div>
+                        <div class="item-content" id="full-name">Stanislav Medvec</div>
+                    </div>
+                    <div class="contact-item">
+                        <div class="item-title">email:</div>
+                        <div class="item-content" id="email"></div>
+                    </div>
+                    <div class="contact-item">
+                        <div class="item-title">github:</div>
+                        <div class="item-content" id="github"></div>
+                    </div>
+                </div>
+            </article>
+        </section>
     </div>
     
 <?php
