d50e3def3e30b6a902700b708aa0240494990b7a
Stanislav Medvec <stanislav.medvec6@gmail.com>
2023-01-20 22:09:16 +0100

    register form, kontroluje vsetko na co som myslel, sifruje heslo, funguje (okrem error textu)

--- 2-tools/E-login/sign-up/sign-up.php
+++ 2-tools/E-login/sign-up/sign-up.php
@@ -30,6 +30,9 @@
         "fname" => false,
         "lname" => false,
         "gender" => false,
+        "age" => false,
+        "height" => false,
+        "weight" => false,
     ];
 
     $classes = [
@@ -41,12 +44,15 @@
         //kontrola emailu
         //trim vystrihne vsetky medzery na zaciatku a konci
         if( trim($_POST["email"]) == "" || 
-        ! (filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)) ) {
+        ! (filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)) 
+        || strlen( trim($_POST["email"]) ) > 60  
+        || SQLquery($conn, "SELECT email FROM users WHERE email='$email' ") ) {
             $errors["email"] = true;
         } 
 
         //kontrola hesla
-        if( trim($_POST["passwd"])  == "" || strlen( trim($_POST["passwd"]) ) < 8 ) {
+        if( trim($_POST["passwd"])  == "" || strlen( trim($_POST["passwd"]) ) < 8 
+        || strlen( trim($_POST["passwd"]) ) > 10 ) {
             $errors["passwd"] = true;
         }
 
@@ -56,23 +62,44 @@
         }
 
         //kontrola mena
-        if( trim($_POST["fname"])  == "" || strlen( trim($_POST["fname"]) ) < 2 ) {
+        if( trim($_POST["fname"])  == "" || strlen( trim($_POST["fname"]) ) < 2 
+        || strlen( trim($_POST["fname"]) ) > 20 ) {
             $errors["fname"] = true;
         }
 
         //kontrola priezviska
-        if( trim($_POST["lname"])  == "" || strlen( trim($_POST["lname"]) ) < 2 ) {
+        if( trim($_POST["lname"])  == "" || strlen( trim($_POST["lname"]) ) < 2 
+        || strlen( trim($_POST["lname"]) ) > 40 ) {
             $errors["lname"] = true;
         }
 
-        //kontrola pohlavie
+        //kontrola pohlavia
         if( $_POST["gender"]  == 0 ) {
             $errors["gender"] = true;
+        }
+
+        //kontrola veku
+        if( $_POST["age"]  == 0 ||  $_POST["age"] < 15 
+        || $_POST["age"] > 150  ) {
+            $errors["age"] = true;
+        }
+
+        //kontrola vysky
+        if( $_POST["height"]  == 0 || $_POST["height"] < 130 
+        || $_POST["height"] > 240 ) {
+            $errors["height"] = true;
+        }
+
+        //kontrola vahy
+        if( $_POST["weight"]  == 0 || $_POST["weight"] < 30 
+        || $_POST["weight"] > 220 ) {
+            $errors["weight"] = true;
         }
     }
 
     if( count($_POST) != 0 && !$errors["email"] && !$errors["passwd"] && !$errors["co_passwd"] 
-    && !$errors["fname"] && !$errors["lname"] && !$errors["gender"] ) {
+    && !$errors["fname"] && !$errors["lname"] && !$errors["gender"] && !$errors["age"]
+    && !$errors["height"] && !$errors["weight"] ) {
         $errors["checked"] = true;
     } else {
         $errors["checked"] = false;
@@ -132,7 +159,7 @@
             <form action="" method="post" id="sign-up-formular">
 
                 <input type="text" id="email" 
-                class="<?php echo addClass( $errors["email"], $classes["eBorder"] ); ?>" 
+                class="<?php echo addClass( $errors["email"], $classes["eBorder"] ); ?>"  
                 name="email" placeholder="E-mail" value="<?= $_POST["email"] ?>">
 
                 <?php //echo addClass( ($errors["email"]),  $eBorder); ?>
@@ -153,16 +180,16 @@
                 class="<?php echo addClass( $errors["co_passwd"], $classes["eBorder"] ); ?>" 
                 name="co_passwd" placeholder="Potvrd heslo" value="<?= $_POST["co_passwd"] ?>">
 
-                <input type="text" id="fname" name="fname" 
-                placeholder="Meno" value="<?= ucfirst($_POST['fname']) ?>"
-                >
+                <input type="text" id="fname" 
+                class="<?php echo addClass( $errors["fname"], $classes["eBorder"] ); ?>" 
+                name="fname" placeholder="Meno" value="<?= ucfirst($_POST['fname']) ?>">
 
-                <input type="text" id="lname" name="lname" 
-                placeholder="Priezvisko" value="<?= ucfirst($_POST['lname']) ?>"
-                >
+                <input type="text" id="lname" 
+                class="<?php echo addClass( $errors["lname"], $classes["eBorder"] ); ?>" 
+                name="lname" placeholder="Priezvisko" value="<?= ucfirst($_POST['lname']) ?>">
 
-                <!-- -->
-                <select id="gender" name="gender" >
+                <select id="gender" name="gender" 
+                class="<?php echo addClass( $errors["gender"], $classes["eBorder"] ); ?>" >
                     <option value="0">Vyber pohlavie</option>
                     <?php foreach($genders as $gender): ?>
                         <option value="<?= $gender["id"] ?>"
@@ -174,18 +201,18 @@
                         </option>
                     <?php endforeach ?>
                 </select>
-                <!-- -->
 
-                <!-- -->
-                <input type="number" id="age" name="age" 
-                placeholder="Vek">
+                <input type="number" id="age" 
+                name="age" placeholder="Vek" value="<?= $_POST['age'] ?>"
+                class="<?php echo addClass( $errors["age"], $classes["eBorder"] ); ?>" >
 
-                <input type="number" id="height" name="height" 
-                placeholder="Vyska">
+                <input type="number" id="height" 
+                name="height" placeholder="Vyska" value="<?= $_POST['height'] ?>"
+                class="<?php echo addClass( $errors["height"], $classes["eBorder"] ); ?>" >
 
-                <input type="number" id="weight" name="weight" 
-                placeholder="Vaha">
-                <!-- -->
+                <input type="number" id="weight" 
+                name="weight" placeholder="Vaha" value="<?= $_POST['weight'] ?>"
+                class="<?php echo addClass( $errors["weight"], $classes["eBorder"] ); ?>" >
 
                 <input type="submit" id="bt-login" name="submit" 
                 value="Register">
