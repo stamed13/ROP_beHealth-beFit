ffc6dbd84d09f3ce34e947f88f272dbdbfe2daa9
student <stanislav.medvec6@gmail.com>
2023-02-01 12:10:32 +0100

    create profile page and header to this section

---
+++ 2-tools/E-login/profile/profile.php
@@ -0,0 +1,29 @@
+<?php
+    //aktualna stranka
+    $currentPage = 'profile';
+
+    //pripojenie header casti
+    include('layout/header.php');
+
+    $_SESSION["location"] = "../../../index.php";
+
+    //session_destroy();
+?>
+
+    <div id="content">
+        <div id="content-title">Home</div>
+        <article id="main">
+            <div id="info">Ahoj, vitaj na stranke beHealth-beFit!</div>
+            <img src="2-tools/B-media/strong-man.jpg" alt="hopa" id="home-sportsman">
+        </article>
+
+    </div>
+    
+<?php
+    //pripojenie footer casti
+    include('../../../C-layout/footer.php');
+?>
+
+</body>
+
+</html>
