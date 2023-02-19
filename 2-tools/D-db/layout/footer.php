b4236591a3cd149667313031829fcbf979aad84d
Stanislav Medvec <stanislav.medvec6@gmail.com>
2023-02-19 12:36:48 +0100

    update advice and db page

---
+++ 2-tools/D-db/layout/footer.php
@@ -0,0 +1,6 @@
+<footer id="footer">
+    <div id="footer-info">
+        <div id="since">Since 2022</div>
+    </div>
+</footer>
+<script src="<?php echo $up ?>../../../../C-layout/login-ham.js"></script>
