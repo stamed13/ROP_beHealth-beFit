69dfdf43bbdf4a3859705ec85fc288cd641d48bd
Stanislav Medvec <stanislav.medvec6@gmail.com>
2023-02-21 23:31:34 +0100

    add skript tabulky advice a uprava CommitAdvice

--- 2-tools/D-db/admin/commit/adviceCommit.php
+++ 2-tools/D-db/admin/commit/adviceCommit.php
@@ -13,20 +13,19 @@
         //ak mam nove udaje pridam tu insert aj update, pripadne ine prikazy
         $sql = "INSERT INTO advice (idAdvice, name) VALUES (1, 'Máš normálnu váhu.')";
         mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (2, 'Máš nadváhu.')";
-        mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (3, 'Máš nízku váhu.')";
-        mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (4, 'Máš obezitu.')";
-        mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (5, 'Tesim sa, ze cvicis dost casto.')";
-        mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (6, 'Tesim sa, cvicis rovnomerne na vsetky partie.')";
-        mysqli_query($conn, $sql);
-        $sql = "INSERT INTO advice (idAdvice, name) VALUES (20, 'row 20')";
+        $sql = "UPDATE advice SET name='Máš normálnu váhu.' WHERE idAdvice=1";
         mysqli_query($conn, $sql);
 
         //
         mysqli_close($conn);
+        //
+        //Skus viac posilovat ruky.
+        //Skus viac posilovat nohy.
+        //Skus viac strecovat chrbat.
+        //Skus viac strecovat nohy.
+        //Ak mas bolesti hlavy alebo zapastia skus prosim strecovat krk alebo ruky (to co robi problem).
+        //Si zaciatocnik, pokojne cvic dalej, vysledky pomaly pridu.
+        //Si pokrocily, este mozes posunut svoje schopnosti.
+        //Si expert, pokojne si zlepsuj.
     }
 ?>
