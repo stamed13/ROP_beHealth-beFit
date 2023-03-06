2bdb7f713627b69f0450770af10e05d15e1c309d
Stanislav Medvec <stanislav.medvec6@gmail.com>
2023-03-06 11:25:48 +0100

    update exercises page design

--- 1-pages/2-exercises/1-exercise/1-calisthenics/core.php
+++ 1-pages/2-exercises/1-exercise/1-calisthenics/core.php
@@ -13,10 +13,12 @@
 ?>
 
     <div id="content">
-        <a href="../../../../exercises.php#calisthenics" id="back">naspäť</a>
-        <section id="type">
+        <section id="type" class="cali">
             <article id="main">
-                <div id="content-title" class="exercise-title">Jadro</div>
+                <div id="main-head">
+                    <a href="../../../../exercises.php#calisthenics" id="back">naspäť</a>
+                    <div id="content-title" class="exercise-title">Jadro</div>
+                </div>
                 <div id="type-info" class="exercise-info">
                     Pri týchto cvikoch posilňujeme svaly jadra ako sú brušné svaly. Veľa ľudí si spája cvičenie na
                     jadro s tehličkami. "Tehličky" na bruchu ukazujú koľko percent podkožného tuku máme. 
@@ -24,22 +26,20 @@
                 </div>
                 <div id="exercises">
                     <div class="ex-kinds-title">cviky:</div>
-                    <div class="ex-kinds-kinds">
-                        <div class="ex-kinds-kind">plank, kolíska, horolezci...</div>
-                    </div>
+                    <div class="ex-kinds-kinds">plank, kolíska, horolezci...</div>
                 </div>
                 <div id="types-of-type">
                     <div id="types-ex-title">Zoznam cvikov na brucho</div>
                     <div id="types-ex-list">
-                        <a href="#beginer" class="type-ex">level začiatočník</a>
-                        <a href="#intermediate" class="type-ex">level pokročilý</a>
-                        <a href="#expert" class="type-ex">level expert</a>
+                        <a href="#beginer" class="type-ex beginer">level začiatočník</a>
+                        <a href="#intermediate" class="type-ex intermediate">level pokročilý</a>
+                        <a href="#expert" class="type-ex expert">level expert</a>
                     </div>
                 </div>
             </article>
 
             <article id="beginer" class="exercise">
-                <div class="exercise-title">Začiatočník</div>
+                <div class="exercise-title beginer">Začiatočník</div>
                 <div class="exercise-info">
                     Level začiatočník je pre úplných začiatočníkov, ale dokáže poriadne potrápiť. 
                     Cviky sú jednoduché na učenie.
@@ -108,7 +108,7 @@
             </article>
 
             <article id="intermediate" class="exercise">
-                <div class="exercise-title">Pokročilý</div>
+                <div class="exercise-title intermediate">Pokročilý</div>
                 <div class="exercise-info">
                     Level pokročilý je pre mierne pokročilých alebo pokročilých. Sú to už náročnejšie cviky.
                 </div>
@@ -200,7 +200,7 @@
             </article>
 
             <article id="expert" class="exercise">
-                <div class="exercise-title">Expert</div>
+                <div class="exercise-title expert">Expert</div>
                 <div class="exercise-info">
                     Level pre skutočných makačov, pre tých ktorý sú už na vysokej úrovni a majú toho veľa natrénovaného.
                 </div>
